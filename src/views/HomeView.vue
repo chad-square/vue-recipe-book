05fee895256f879c7cb1fb1952de8abf5946b143
chad <chad.square1@gmail.com>
2024-03-18 18:46:56 +0200

    removed starter boilerplate code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home">
+    <h1>This is an about page</h1>
   </main>
 </template>
