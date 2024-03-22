d61ee1e73e6f35e5962c306f3a1b64f1dac91133
chad <chad.square1@gmail.com>
2024-03-22 23:45:14 +0200

    removed supabaseClient

--- src/lib/subaseClient.ts
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import {supabaseKey, supabaseUrlPrefix} from "@/data";
-
-export const supabase = createClient(`https://${supabaseUrlPrefix}.supabase.co`, supabaseKey)
