cb453abb28bc3c85e8e374e72eb3c575006145ed
chad <chad.square1@gmail.com>
2024-05-04 23:41:38 +0200

    updated InstructionsInput.vue

--- src/components/cook-book/create-recipe/InstructionsInput.vue
+++ src/components/cook-book/create-recipe/InstructionsInput.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import {ref} from "vue";
-import {requiredValidation} from "@/shared/form/formValidations";
 
 const instructions = defineModel<string[]>()
 const instructionInput = ref('')
 
 const onAddInstruction = function () {
   if (instructionInput.value.length) {
-    instructions.value?.unshift(instructionInput.value)
+    instructions.value?.push(instructionInput.value)
     instructionInput.value = '';
   }
 }
@@ -16,15 +15,20 @@ const onAddInstruction = function () {
 
 <template>
   <div class="form-control instructions-control">
-    <div class="inputs-container">
-      <Textarea placeholder="Add Instruction..." v-model="instructionInput"/>
-      <Button @click="onAddInstruction" class="pi pi-plus"></Button>
-    </div>
-    <ul class="recipe-input-list">
+
+    <ul class="recipe-input-list instruction-list">
       <li v-for="(instruction,  index) in instructions" :key="index">
         {{ instruction }}
       </li>
     </ul>
+
+
+
+    <div class="inputs-container">
+      <Textarea placeholder="Add Instruction..." v-model="instructionInput"/>
+      <Button @click="onAddInstruction" class="pi pi-plus"></Button>
+    </div>
+
   </div>
 </template>
 
@@ -33,6 +37,24 @@ const onAddInstruction = function () {
 
 .inputs-container {
 
+}
+
+.instruction-list {
+  padding: 30px;
+  height: 20vh;
+  text-align: center;
+  overflow-y: auto;
+  border-radius: 5px;
+  width: 100%;
+
+  -webkit-box-shadow: 0px 0px 1px 1px rgba(177,115,95,0.52);
+  -moz-box-shadow: 0px 0px 1px 1px rgba(177,115,95,0.52);
+  box-shadow: 0px 0px 1px 1px rgba(177,115,95,0.52);
+
+  li {
+    margin: 5px;
+
+  }
 }
 
 /* for larger than mobile */
