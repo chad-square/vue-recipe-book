2148c7992ffebfbb46074eeefc9934b07c62bfe3
chad <chad.square1@gmail.com>
2024-05-14 01:21:46 +0200

    edit recipe - just to remember to structure

--- src/models/RecipeMetadata.ts
+++ src/models/RecipeMetadata.ts
@@ -1,3 +1,17 @@
+// export interface RecipeMetadata {
+//     id: number | undefined;
+//     recipeId: number | undefined;
+//     name: string;
+//     author: string;
+//     likes: [34, 27, 31, 8] | "[\"34\",\"27\",\"31\",\"8\"]";
+//     thumbnailPath: string;
+//     isPrivate: false;
+//     categories: ["Dessert", "Side"] | "[\"Dessert\",\"Side\"]";
+//     cookingTime: string;
+// }
+
+
+
 export interface RecipeMetadata {
     id: number | undefined;
     recipeId: number | undefined;
